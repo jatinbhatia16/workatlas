b61cc480f3bdc3faa7fc63f6707031d94f86ed5f
JATIN BHATIA <jatinbhatia@JATINs-MacBook-Air.local>
2026-01-31 01:48:54 +1100

    For auto approval importing currentdate and time

--- src/main/java/in/workatlas/service/ProfessionalService.java
+++ src/main/java/in/workatlas/service/ProfessionalService.java
@@ -10,7 +10,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
+import java.time.LocalDateTime;
 import java.time.Instant;
 
 @Service
